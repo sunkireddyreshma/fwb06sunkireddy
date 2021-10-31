62f4f6c9afea459ddf5dd17da03b7248d48e01e2
Reshma <sunkireddyreshma1999@gmail.com>
2021-10-31 18:01:58 -0500

    commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var horseRouter = require('./routes/horse');
 
 var app = express();
 
